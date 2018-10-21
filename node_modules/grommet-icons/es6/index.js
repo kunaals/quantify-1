a4bcd2e297ac402a982b12f327f7b6d3c29f5cec
Paari Gopal <paarig@cs.washington.edu>
2018-10-20 17:39:53 -0700

    First commit

---
+++ node_modules/grommet-icons/es6/index.js
@@ -0,0 +1,7 @@
+import StyledIcon from './StyledIcon';
+
+export * from './icons';
+export { default as Blank } from './Blank';
+export { default as ThemeContext } from './ThemeContext';
+
+export default StyledIcon;
